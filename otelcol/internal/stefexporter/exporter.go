4c4b3f90192ce20152a77416c372e0326d99867c
Tigran Najaryan <tnajaryan@splunk.com>
2025-03-03 16:24:37 -0500

    Improve STEF exporter
    - Update STEF exporter to the latest one from Otel Collector repo.
    - Add ConnManager and Sync2Async helpers.

--- otelcol/internal/stefexporter/exporter.go
+++ otelcol/internal/stefexporter/exporter.go
@@ -1,261 +1,154 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-package stefexporter // import "github.com/splunk/stef/otelcol/internal/stefexporter"
+package stefexporter // import "github.com/open-telemetry/opentelemetry-collector-contrib/exporter/stefexporter"
 
 import (
 	"context"
-	"fmt"
-	"log"
-	"os"
-	"sync"
 	"time"
 
-	"github.com/gogo/protobuf/proto"
-	"github.com/open-telemetry/otel-arrow/pkg/config"
-	"github.com/open-telemetry/otel-arrow/pkg/otel/arrow_record"
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/pdata/pmetric"
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
 
-	"github.com/splunk/stef/go/pkg"
-
-	tef_grpc "github.com/splunk/stef/go/grpc"
-	"github.com/splunk/stef/go/grpc/stef_proto"
-	tef_grpctypes "github.com/splunk/stef/go/grpc/types"
-	"github.com/splunk/stef/go/otel/oteltef"
-	otlpconvert "github.com/splunk/stef/go/pdata/metrics"
-	"github.com/splunk/stef/go/pdata/metrics/sortedbymetric"
+	stefpdatametrics "github.com/splunk/stef/go/pdata/metrics"
+	stefpkg "github.com/splunk/stef/go/pkg"
+	"github.com/splunk/stef/otelcol/internal/stefexporter/internal"
 )
 
+// stefExporter implements sending metrics over STEF/gRPC stream.
+//
+// The exporter uses a single stream and accepts concurrent exportMetrics calls,
+// sequencing the metric data as needed over a single stream.
+//
+// The exporter will block exportMetrics call until an acknowledgement is
+// received from destination.
+//
+// The exporter relies on a preceding Retry helper to retry sending data that is
+// not acknowledged or otherwise fails to be sent. The exporter will not retry
+// sending the data itself.
 type stefExporter struct {
-	logger *zap.Logger
+	set         component.TelemetrySettings
+	cfg         Config
+	compression stefpkg.Compression
 
-	cfg *Config
+	grpcConn *grpc.ClientConn
 
-	arrowFile     *os.File
-	arrowProducer *arrow_record.Producer
-
-	grpcConn     *grpc.ClientConn
-	writeMutex   sync.Mutex
-	remoteWriter *oteltef.MetricsWriter
-
-	lastSent time.Time
-
-	// Ack fields
-	ackMutex          sync.Mutex
-	lastSentRecordId  uint64
-	lastAckedRecordId uint64
-	sentPendingAck    map[uint64]*sortedbymetric.SortedTree
-	stopped           chan struct{}
+	connMan    *internal.ConnManager
+	sync2Async *internal.Sync2Async
 }
 
-type wrapLogger struct {
-	logger *zap.Logger
-}
+const flushPeriod = 100 * time.Millisecond
+const reconnectPeriod = 10 * time.Minute
 
-func (w *wrapLogger) Debugf(ctx context.Context, format string, v ...interface{}) {
-	w.logger.Debug(fmt.Sprintf(format, v...))
-}
-
-func (w *wrapLogger) Errorf(ctx context.Context, format string, v ...interface{}) {
-	w.logger.Error(fmt.Sprintf(format, v...))
-}
-
-var _ tef_grpctypes.Logger = (*wrapLogger)(nil)
-
-func newStefExporter(logger *zap.Logger, cfg *Config) *stefExporter {
-	return &stefExporter{
-		logger:         logger,
-		cfg:            cfg,
-		sentPendingAck: map[uint64]*sortedbymetric.SortedTree{},
-		stopped:        make(chan struct{}),
+func newStefExporter(set component.TelemetrySettings, cfg *Config) *stefExporter {
+	exp := &stefExporter{
+		set: set,
+		cfg: *cfg,
 	}
+
+	exp.compression = stefpkg.CompressionNone
+	if exp.cfg.Compression == "zstd" {
+		exp.compression = stefpkg.CompressionZstd
+	}
+	// Disable built-in grpc compression. STEF has its own zstd compression support.
+	exp.cfg.Compression = ""
+	return exp
 }
 
 func (s *stefExporter) Start(ctx context.Context, host component.Host) error {
+	// Prepare gRPC connection.
 	var err error
-	if s.cfg.ArrowPath != "" {
-		s.arrowFile, err = os.Create(s.cfg.ArrowPath)
-		if err != nil {
-			return err
-		}
-	}
-
-	compression := pkg.CompressionNone
-	if s.cfg.Compression == "zstd" {
-		compression = pkg.CompressionZstd
-	}
-
-	if err := s.startGrpcClient(compression); err != nil {
+	s.grpcConn, err = s.cfg.ClientConfig.ToClientConn(ctx, host, s.set)
+	if err != nil {
 		return err
 	}
 
-	var opts []config.Option
-	if s.cfg.Compression == "zstd" {
-		opts = append(opts, config.WithZstd())
-	} else {
-		opts = append(opts, config.WithNoZstd())
-	}
-	s.arrowProducer = arrow_record.NewProducerWithOptions(opts...)
+	connCreator := internal.NewStefConnCreator(s.set.Logger, s.grpcConn, s.compression)
+	s.connMan = internal.NewConnManager(s.set.Logger, connCreator, 1, flushPeriod, reconnectPeriod)
+	s.connMan.Start()
 
+	s.sync2Async = internal.NewSync2Async(s.set.Logger, s.cfg.NumConsumers, s.exportMetricsAsync)
+
+	// No need to block Start(), we will begin connection attempt in a goroutine.
+	go func() {
+		conn, err := s.connMan.Acquire(ctx)
+		if err != nil {
+			s.set.Logger.Error("Error connecting to destination", zap.Error(err))
+			// This is not a fatal error. exportMetrics() will try to connect again as needed.
+			return
+		}
+		s.connMan.Release(conn)
+	}()
 	return nil
 }
 
 func (s *stefExporter) Shutdown(ctx context.Context) error {
-	close(s.stopped)
-	if s.arrowFile != nil {
-		s.arrowFile.Close()
+	if err := s.connMan.Stop(ctx); err != nil {
+		s.set.Logger.Error("Failed to stop connection manager", zap.Error(err))
 	}
+
 	if s.grpcConn != nil {
-		return s.grpcConn.Close()
+		if err := s.grpcConn.Close(); err != nil {
+			s.set.Logger.Error("Failed to close grpc connection", zap.Error(err))
+		}
+		s.grpcConn = nil
 	}
 	return nil
 }
 
-func (s *stefExporter) pushMetrics(_ context.Context, md pmetric.Metrics) error {
-	s.logger.Info(
-		"MetricsExporter",
-		zap.Int("resource metrics", md.ResourceMetrics().Len()),
-		zap.Int("metrics", md.MetricCount()),
-		zap.Int("data points", md.DataPointCount()),
-	)
-
-	converter := otlpconvert.NewOtlpToSortedTree()
-	sorted, err := converter.FromOtlp(md.ResourceMetrics())
-	if err != nil {
-		return err
-	}
-
-	s.writeMutex.Lock()
-	defer s.writeMutex.Unlock()
-
-	if s.arrowFile != nil {
-		records, err := s.arrowProducer.BatchArrowRecordsFromMetrics(md)
-		if err != nil {
-			return err
-		}
-
-		arrowBytes, err := proto.Marshal(records)
-		_, err = s.arrowFile.Write(arrowBytes)
-	}
-
-	if s.remoteWriter != nil {
-		err := sorted.ToTef(s.remoteWriter)
-		if err != nil {
-			return err
-		}
-
-		s.ackMutex.Lock()
-		s.lastSentRecordId = s.remoteWriter.RecordCount()
-		if s.lastAckedRecordId >= s.lastSentRecordId {
-			// Already received by destination and acknowledged. Can happen if onGrpcAck() is called
-			// before we even manage to add it to the map here. No need to add it to the map anymore.
-			// It is acknowledged and safe.
-		} else {
-			s.sentPendingAck[s.lastSentRecordId] = sorted
-		}
-		s.ackMutex.Unlock()
-	}
-
-	return nil
+func (s *stefExporter) exportMetrics(ctx context.Context, data pmetric.Metrics) error {
+	return s.sync2Async.Sync(ctx, data)
 }
 
-func (s *stefExporter) flusher() {
-	timer := time.NewTicker(100 * time.Millisecond)
-	for {
-		select {
-		case <-timer.C:
-			s.writeMutex.Lock()
-			err := s.remoteWriter.Flush()
-			s.writeMutex.Unlock()
-			if err != nil {
-				log.Printf("Cannot send STEF data: %v\n", err)
-			}
-		case <-s.stopped:
-			return
-		}
-	}
-}
-
-func (s *stefExporter) onGrpcAck(ackId uint64) error {
-	s.ackMutex.Lock()
-	defer s.ackMutex.Unlock()
-	for ; s.lastAckedRecordId < ackId; s.lastAckedRecordId++ {
-		delete(s.sentPendingAck, s.lastAckedRecordId)
-	}
-	return nil
-}
-
-func (s *stefExporter) startGrpcClient(compression pkg.Compression) error {
-	// Connect to the server.
-	var err error
-	s.grpcConn, err = grpc.NewClient(
-		fmt.Sprintf(s.cfg.Endpoint),
-		grpc.WithTransportCredentials(insecure.NewCredentials()),
-	)
+func (s *stefExporter) exportMetricsAsync(
+	ctx context.Context,
+	data any,
+	resultChan internal.ResultChan,
+) (internal.DataID, error) {
+	conn, err := s.connMan.Acquire(ctx)
 	if err != nil {
-		return err
+		return 0, err
 	}
 
-	// Open a stream to the server.
-	grpcClient := stef_proto.NewSTEFDestinationClient(s.grpcConn)
+	stefConn := conn.Conn().(*internal.StefConn)
+	stefWriter := stefConn.Writer()
 
-	schema, err := oteltef.MetricsWireSchema()
+	md := data.(pmetric.Metrics)
+
+	converter := stefpdatametrics.OtlpToSTEFUnsorted{}
+
+	err = converter.WriteMetrics(md, stefWriter)
 	if err != nil {
-		return err
+		s.set.Logger.Debug("WriteMetrics failed", zap.Error(err))
+
+		// Error to write to STEF stream typically indicates either:
+		// 1) A problem with the connection. We need to reconnect.
+		// 2) Encoding failure, possibly due to encoder bug. In this case
+		//    we need to reconnect too, to make sure encoders start from
+		//    initial state, which is our best chance to succeed next time.
+		//
+		// We need to reconnect. Disconnect here and the next exportMetrics()
+		// call will connect again.
+		s.connMan.DiscardAndClose(conn)
+
+		// TODO: check if err is because STEF encoding failed. If so we must not
+		// try to re-encode the same data. Return consumererror.NewPermanent(err)
+		// to the caller. This requires changes in STEF Go library.
+
+		// Return an error to retry sending these metrics again next time.
+		return 0, err
 	}
 
-	settings := tef_grpc.ClientSettings{
-		Logger:       &wrapLogger{s.logger},
-		GrpcClient:   grpcClient,
-		ClientSchema: &schema,
-		Callbacks: tef_grpc.ClientCallbacks{
-			OnAck: s.onGrpcAck,
-		},
-	}
-	client := tef_grpc.NewClient(settings)
+	// According to STEF gRPC spec the destination ack IDs match written record number.
+	// When the data we have just written is received by destination it will send us
+	// back and ack ID that numerically matches the last written record number.
+	expectedAckID := stefWriter.RecordCount()
 
-	grpcWriter, opts, err := client.Connect(context.Background())
+	stefConn.OnAck(expectedAckID, resultChan)
 
-	opts.Compression = compression
+	s.connMan.Release(conn)
 
-	// Create a byte writer over the stream.
-	//grpcWriter, err := tef_grpc.NewGrpcWriter(context.Background(), grpcClient, s.onGrpcAck)
-	if err != nil {
-		return err
-	}
-
-	// Create record writer.
-	s.remoteWriter, err = oteltef.NewMetricsWriter(grpcWriter, opts)
-	if err != nil {
-		return err
-	}
-	go s.flusher()
-
-	return err
-}
-
-func clearUnsupportedMetricTypes(md pmetric.Metrics) {
-	for i := 0; i < md.ResourceMetrics().Len(); i++ {
-		rm := md.ResourceMetrics().At(i)
-		for j := 0; j < rm.ScopeMetrics().Len(); j++ {
-			sms := rm.ScopeMetrics().At(j)
-			sms.Metrics().RemoveIf(
-				func(metric pmetric.Metric) bool {
-					switch metric.Type() {
-					case pmetric.MetricTypeGauge:
-						return false
-					case pmetric.MetricTypeSum:
-						return false
-					default:
-						return true
-					}
-				},
-			)
-		}
-	}
-
+	return internal.DataID(expectedAckID), nil
 }
--- otelcol/internal/stefexporter/factory_test.go
+++ otelcol/internal/stefexporter/factory_test.go
@@ -8,9 +8,10 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-
 	"go.opentelemetry.io/collector/component/componenttest"
 	"go.opentelemetry.io/collector/exporter/exportertest"
+
+	"github.com/splunk/stef/otelcol/internal/stefexporter/internal/metadata"
 )
 
 func TestCreateDefaultConfig(t *testing.T) {
@@ -24,7 +25,7 @@ func TestCreateMetricsExporter(t *testing.T) {
 	factory := NewFactory()
 	cfg := factory.CreateDefaultConfig()
 
-	me, err := factory.CreateMetrics(context.Background(), exportertest.NewNopSettings(), cfg)
+	me, err := factory.CreateMetrics(context.Background(), exportertest.NewNopSettings(metadata.Type), cfg)
 	assert.NoError(t, err)
 	assert.NotNil(t, me)
 }
