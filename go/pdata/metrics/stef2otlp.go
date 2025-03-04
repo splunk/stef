be6e476c9a8638a09ee9f4950a50b28a992c54ef
Tigran Najaryan <4194920+tigrannajaryan@users.noreply.github.com>
2025-03-03 20:51:51 -0500

    Add basic STEF receiver implementation (#50)
    Added ConvertTillEndOfFrame() converter that ensures that we
    won't read a record and keep it in memory while blocked on
    further reads. Instead all read data is pushed to the pipeline
    by STEF receiver quickly after decoding. To make this possible,
    introduced a new TillEndOfFrame ReadOption.
    
    I upgraded to new Collector version, that's why lots of go.mod/god.sum
    changes and fixes in the stefexporter implementation due to breaking
    API changes in the Collector.
    
    Used Copilot to write a small portion of ConvertTillEndOfFrame().
    
    Tested implementation with STEF exporter from Collector:
    https://github.com/open-telemetry/opentelemetry-collector-contrib/tree/main/exporter/stefexporter

--- go/pdata/metrics/stef2otlp.go
+++ go/pdata/metrics/stef2otlp.go
@@ -66,3 +66,71 @@ func (c *STEFToOTLPUnsorted) Convert(reader *oteltef.MetricsReader) (pmetric.Met
 
 	return metrics, nil
 }
+
+// ConvertTillEndOfFrame reads at least one record and then continues reading
+// records from the reader until the end of the current frame and converts
+// them to pmetric.Metrics.
+//
+// Only the very first record reading may block on I/O. Once the first record
+// is read, the subsequent reads will not block on I/O. This is useful for
+// converting records from a Reader that uses an underlying GrpcReader that
+// can block on reading from network. This ensures that we won't read a record
+// and keep it in memory while blocked on further reads.
+func (c *STEFToOTLPUnsorted) ConvertTillEndOfFrame(reader *oteltef.MetricsReader) (pmetric.Metrics, error) {
+	var resourceMetrics pmetric.ResourceMetrics
+	var scopeMetrics pmetric.ScopeMetrics
+	var metric pmetric.Metric
+
+	metrics := pmetric.NewMetrics()
+
+	// Read the first record. This call may block on I/O.
+	err := reader.Read(pkg.ReadOptions{})
+	if err != nil {
+		return metrics, err
+	}
+	record := &reader.Record
+
+	modified := true
+	for {
+		if modified || record.IsResourceModified() {
+			modified = true
+			resourceMetrics = metrics.ResourceMetrics().AppendEmpty()
+			if err := otlptools.ResourceToOtlp(record.Resource(), resourceMetrics); err != nil {
+				return metrics, err
+			}
+		}
+
+		if modified || record.IsScopeModified() {
+			modified = true
+			scopeMetrics = resourceMetrics.ScopeMetrics().AppendEmpty()
+			if err := otlptools.ScopeToOtlp(record.Scope(), scopeMetrics); err != nil {
+				return metrics, err
+			}
+		}
+
+		if modified || record.IsMetricModified() {
+			metric = scopeMetrics.Metrics().AppendEmpty()
+			if err := otlptools.MetricToOtlp(record.Metric(), metric); err != nil {
+				return metrics, err
+			}
+		}
+
+		err = c.AppendOTLPPoint(record.Metric(), record.Attributes(), record.Point(), metric)
+		if err != nil {
+			return metrics, err
+		}
+
+		modified = false
+
+		// Read more records. This will not block on I/O.
+		err = reader.Read(pkg.ReadOptions{TillEndOfFrame: true})
+		if err != nil {
+			if errors.Is(err, pkg.ErrEndOfFrame) {
+				break
+			}
+			return metrics, err
+		}
+	}
+
+	return metrics, nil
+}
