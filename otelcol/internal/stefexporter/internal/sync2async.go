fbb752051f3af0d7263e72e8b2d1856082bcaec8
Tigran Najaryan <tnajaryan@splunk.com>
2025-03-04 19:19:56 -0500

    Add error to AsyncFunc result

--- otelcol/internal/stefexporter/internal/sync2async.go
+++ otelcol/internal/stefexporter/internal/sync2async.go
@@ -8,73 +8,78 @@ import (
 )
 
 type DataID uint64
-type AckChan chan DataID
+
+type AsyncResult struct {
+	DataID DataID
+	Err    error
+}
+type ResultChan chan AsyncResult
 
 type Async func(
 	ctx context.Context,
 	data any,
-	ackChan AckChan,
+	resultChan ResultChan,
 ) (DataID, error)
 
 type Sync2Async struct {
-	logger          *zap.Logger
-	async           Async
-	asyncMux        sync.Mutex
-	ackChannelsRing chan chan DataID
+	logger             *zap.Logger
+	async              Async
+	asyncMux           sync.Mutex
+	resultChannelsRing chan chan AsyncResult
 }
 
 func NewSync2Async(logger *zap.Logger, concurrency int, async Async) *Sync2Async {
 	s := &Sync2Async{
-		logger:          logger,
-		async:           async,
-		ackChannelsRing: make(chan chan DataID, concurrency),
+		logger:             logger,
+		async:              async,
+		resultChannelsRing: make(chan chan AsyncResult, concurrency),
 	}
 
 	for i := 0; i < concurrency; i++ {
-		s.ackChannelsRing <- make(chan DataID)
+		s.resultChannelsRing <- make(chan AsyncResult)
 	}
 
 	return s
 }
 
 func (s *Sync2Async) Sync(ctx context.Context, data any) error {
-	// Choose an ackChannel. We are simply doing round-robin here, every Sync()
-	var ackChannel chan DataID
+	// Choose an resultChan. We are simply doing round-robin here, every Sync()
+	var resultChan chan AsyncResult
 	select {
-	case ackChannel = <-s.ackChannelsRing:
+	case resultChan = <-s.resultChannelsRing:
 	case <-ctx.Done():
 		return ctx.Err()
 	}
 	defer func() {
-		s.ackChannelsRing <- ackChannel
+		s.resultChannelsRing <- resultChan
 	}()
 
-	dataID, err := s.async(ctx, data, ackChannel)
+	dataID, err := s.async(ctx, data, resultChan)
 	if err != nil {
 		return err
 	}
 	//fmt.Printf("Wait Ack %04d\n", dataID)
 
 	select {
-	case id := <-ackChannel:
+	case result := <-resultChan:
 		//fmt.Printf("Got  Ack %04d\n", id)
-		if id != dataID {
+		if result.DataID != dataID {
 			// Received ack on the wrong data item. This should normally not happen and indicates a bug somewhere.
 			s.logger.Error(
 				"Received ack on the wrong data item",
 				zap.Uint64("expected", uint64(dataID)),
-				zap.Uint64("actual", uint64(id)),
+				zap.Uint64("actual", uint64(result.DataID)),
 			)
 		}
+		return result.Err
+
 	case <-ctx.Done():
 		// Abandon the ack channel that was given to async() because we don't know when/if
-		// that channel will fire. Just allocate a new channel. The new ackChannel will
-		// be returned to ackChannelsRing when this func returns.
+		// that channel will fire. Just allocate a new channel. The new resultChan will
+		// be returned to resultChannelsRing when this func returns.
 		// If after this ack() is called, it will push dataID on an abandoned channel
 		// and will have no effect.
-		ackChannel = make(chan DataID)
+		resultChan = make(chan AsyncResult)
 		return ctx.Err()
 	}
-
-	return nil
 }
