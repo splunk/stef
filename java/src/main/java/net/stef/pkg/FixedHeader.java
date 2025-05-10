e1728711f5ba089e33012f413bca3a71fd2f6772
Tigran Najaryan <tnajaryan@splunk.com>
2025-05-10 12:56:15 -0400

    Add VarHeader

---
+++ java/src/main/java/net/stef/pkg/FixedHeader.java
@@ -0,0 +1,13 @@
+package net.stef.pkg;
+
+public class FixedHeader {
+    private Compression compression;
+
+    public Compression getCompression() {
+        return compression;
+    }
+
+    public void setCompression(Compression compression) {
+        this.compression = compression;
+    }
+}
