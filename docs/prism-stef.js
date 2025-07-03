0c63295cc566d1bcaa08446d05cb0f94015cbe91
Tigran Najaryan <4194920+tigrannajaryan@users.noreply.github.com>
2025-07-03 12:35:08 -0400

    Add basic index.html (#100)
    - Show a simple example with json-like schema, generating serializers and writing record in Go.
    - Used prismjs for syntax highlighting.

---
+++ docs/prism-stef.js
@@ -0,0 +1,8 @@
+Prism.languages.stef = {
+  'comment': /\/\/.*$/m,
+  'keyword': /\b(struct|multimap|oneof|enum|package|key|value)\b/,
+  'type': /\b(string|bytes|uint64|int64|float64|bool)\b/,
+  'attribute': /\b(dict|root|optional)\b/,
+  'number': /\b\d+(?:\.\d+)?\b/,
+};
+
