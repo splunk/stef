ed58a5f812402fdd7c4a6fe5bf62eb57d3e8f9fc
Tigran Najaryan <tnajarya@cisco.com>
2025-11-19 07:56:29 +0530

    Make sure optional in struct in multimap works
    Fixes https://github.com/splunk/stef/issues/251
    
    We need to reset optional fields which are structs
    when they were not present previously and are now
    present.

--- go/pkg/schema/recursestack.go
+++ go/pkg/schema/recursestack.go
@@ -5,7 +5,14 @@ type recursable interface {
 }
 
 type recurseStack struct {
-	fields  []recursable
+	// Names of types in the path of the type tree currently being searched
+	// from top to bottom of the tree. Root struct is the first element.
 	asStack []string
-	asMap   map[string]bool
+
+	// Same types, but as a map for fast search by name.
+	asMap map[string]bool
+
+	// Fields of types through which the types in asStack are linked.
+	// fields[i] is a field in asStack[i] type.
+	fields []recursable
 }
