5c72554066160af0af15e58130fd4ab1ef5ff52b
Abhishek Chatterjee <abchatt2@cisco.com>
2025-12-09 23:46:11 +0530

    Implement seed storing for Java schema tests

--- java/src/test/java/tests/ReadWriteTest.java
+++ java/src/test/java/tests/ReadWriteTest.java
@@ -21,7 +21,8 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 
 class ReadWriteTest {
