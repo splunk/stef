9b6347b6420be8be3708a68fb0355c040d00c6f0
Tigran Najaryan <tnajarya@cisco.com>
2025-12-18 18:07:59 -0500

    Add decode allocation size limit
    Contributes to https://github.com/splunk/stef/issues/305
    
    All allocation sizes are now tracked. During decoding the cumulative
    allocation size for the current record is checked against a limit
    and decoding is aborted if the limit is exceeded.
    
    This fixes numerous bugs in places where for example the decoder
    decode a very large length for an array length and tries to allocate
    that array.
    
    The bug was caught by fuzzer and corresponding regressions fuzz test
    is included in the commit.

--- go/pkg/limits.go
+++ go/pkg/limits.go
@@ -4,3 +4,7 @@ const MultimapElemCountLimit = 1024
 
 const FixedHdrContentSizeLimit = 1 << 20
 const VarHdrContentSizeLimit = 1 << 20
+
+// RecordAllocLimit defines the maximum number of bytes that can be allocated
+// during the reading of a single record.
+const RecordAllocLimit = 1 << 25 // 32 MiB
